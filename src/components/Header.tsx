20cdacf43ccffa8fe0e46d5c1fe1a39522a5333a
Philipp Dowling <philipp.dowling@gmail.com>
2021-08-14 22:52:27 -0400

    add components folder

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {    
+    return (
+    <div style={{width: "50%", margin: "auto"}}>
+        <h1>Philipp Dowling</h1>
+        
+        <hr/>
+        </div>
+        );
+    }
+    
+    export default Header;
+    
