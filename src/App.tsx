4a83f669701bf6d1a13cdbb924bb18c551b0ad68
Philipp Dowling <philipp.dowling@gmail.com>
2021-08-14 22:48:36 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+        <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save.
         </p>
         <a
           className="App-link"
