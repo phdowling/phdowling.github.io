a9d29867222a24f13af40e91a301854c7e1f3ae4
Philipp Dowling <phdowling@users.noreply.github.com>
2022-03-10 17:02:49 +0100

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,19 +3,17 @@ import ReactMarkdown from "react-markdown";
 import {ContentPage, NoRefLink} from "./ContentPage";
 
 const content = `## About me
-I'm currently a product manager at **[Maltego](https://maltego.com)**, 
-where I'm working on solutions in and around:
+I currently manage the product team at **[Y42](https://y42.com)**, working on building a next-generation data platform that anyone can run.
 
-- data integration
-- investigative link analysis and automation
-- data sources, data providers and open-source intelligence (OSINT)
-- cybersecurity operations, fraud, AML, dark web and other areas
+Some topics I've worked on and/or have an interest in:
+- data integration, pipelines, analytics, automation and operationalization
+- natural language processing, machine learning and knowledge graphs
+- OSINT, cybersecurity operations, fraud, AML, dark web and other investigative areas
 
-I'm also interested in natural language processing, machine learning and knowledge graphs, 
-especially in how these can be leveraged to build tools that help us better understand the world, 
+I'm especially interested in the intersection of these areas and how these tools can be leveraged to build systems that help us to both better understand the world
 as well as to map that understanding.
 
-In the past, I co-founded **[metris.io](https://metris.io/)** 
+In the past, I worked on integrations and data product at [Maltego](maltego.com), co-founded **[metris.io](https://metris.io/)** 
 and before that, I was working on NLP research around distributional semantic representation of text.
 `;
 
